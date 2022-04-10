3799a72301b1a7d6b2b20d9227ef8c9e268357b4
thuannguyen88 <getaholdofthuan@gmail.com>
2022-04-10 11:43:12 +0100

    installed heroicons for tailwind and imported menuicon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <>
-      <h1 className="text-4xl">Hello</h1>
+      <Navbar />
+      <h1 className="text-4xl text-center">Hello</h1>
     </>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
+
+export default function Navbar() {
+  return (
+    <div>
+      <MenuIcon />
+    </div>
+  );
+}
