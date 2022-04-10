78d7c6ef807c1e7ccdf18b40a4bace866ca161e0
thuannguyen88 <getaholdofthuan@gmail.com>
2022-04-10 11:50:48 +0100

    building out navbar with tailwind css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,11 @@ import { MenuIcon, XIcon } from "@heroicons/react/outline";
 
 export default function Navbar() {
   return (
-    <div>
-      <MenuIcon />
+    <div className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
+      <div className="px-2 flex justify-between items-center w-full h-full">
+        <div>BRAND.</div>
+      </div>
+      <MenuIcon className="w-5" />
     </div>
   );
 }
