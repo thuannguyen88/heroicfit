db3ef63a091b1e8d639db0fdd33d791b9de35041
thuannguyen88 <getaholdofthuan@gmail.com>
2022-04-15 14:26:02 +0100

    create nav bar iems and media queries for displaying and hiding navbar items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,34 @@ export default function Navbar() {
   return (
     <div className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
       <div className="px-2 flex justify-between items-center w-full h-full">
-        <div>BRAND.</div>
+        <div className="flex items-center">
+          <h1 className="text-3xl font-bold mr-4 sm:text-4xl">HEROICFIT.</h1>
+          <ul className="hidden md:flex ">
+            <li>home</li>
+            <li>about</li>
+            <li>support</li>
+            <li>platforms</li>
+            <li>pricing</li>
+          </ul>
+        </div>
+        <div className="hidden md:flex pr-4">
+          <button className="border-none bg-transparent text-black mr-4">
+            Sign In
+          </button>
+          <button className="px-4 py-2">Sign Up</button>
+        </div>
+        <div className="md:hidden">
+          <MenuIcon className="w-12" />
+        </div>
       </div>
-      <MenuIcon className="w-5" />
+
+      <ul className="absolute w-full px-8 bg-zinc-200">
+        <li className>home</li>
+        <li className>about</li>
+        <li className>support</li>
+        <li className>platforms</li>
+        <li className>pricing</li>
+      </ul>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "640px",
+
+      md: "768px",
+
+      lg: "1024px",
+
+      xl: "1280px",
+
+      "2xl": "1536px",
+      // extend: {},
+    },
   },
   plugins: [],
-}
+};
