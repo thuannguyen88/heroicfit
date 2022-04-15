59d8f4b6a7397c561e4ecfeec2ce81186a16829c
thuannguyen88 <getaholdofthuan@gmail.com>
2022-04-15 15:41:02 +0100

    set up useState, handleClick and conditional rendering for css to open and close navbar menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 
 export default function Navbar() {
+  const [navMenu, setNavMenu] = useState(false);
+  const handleClick = () => {
+    setNavMenu(!navMenu);
+  };
+
   return (
     <div className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
       <div className="px-2 flex justify-between items-center w-full h-full">
@@ -21,17 +26,24 @@ export default function Navbar() {
           </button>
           <button className="px-4 py-2">Sign Up</button>
         </div>
-        <div className="md:hidden">
-          <MenuIcon className="w-12" />
+        <div className="md:hidden" onClick={handleClick}>
+          {navMenu ? <MenuIcon className="w-12" /> : <XIcon className="w-5" />}
+          {/* // <MenuIcon className="w-12" /> */}
         </div>
       </div>
 
-      <ul className="absolute w-full px-8 bg-zinc-200">
-        <li className>home</li>
-        <li className>about</li>
-        <li className>support</li>
-        <li className>platforms</li>
-        <li className>pricing</li>
+      <ul className={navMenu ? "hidden" : "absolute w-full px-8 bg-zinc-200"}>
+        <li className="border-b-2 border-zinc-300 w-full">home</li>
+        <li className="border-b-2 border-zinc-300 w-full">about</li>
+        <li className="border-b-2 border-zinc-300 w-full">support</li>
+        <li className="border-b-2 border-zinc-300 w-full">platforms</li>
+        <li className="border-b-2 border-zinc-300 w-full">pricing</li>
+        <div className="flex flex-col my-2">
+          <button className="bg-transparent text-indigo-600 px-4 py-2 my-5">
+            Sign In
+          </button>
+          <button className="px-4 py-2">Sign Up</button>
+        </div>
       </ul>
     </div>
   );
