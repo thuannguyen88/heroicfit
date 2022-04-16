5e58319b1aff97b2bafa3ecaac7951af44db33f5
thuannguyen88 <getaholdofthuan@gmail.com>
2022-04-16 12:29:42 +0100

    edit css spacing for mission component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Benefits from "./components/Benefits";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Benefits />
     </>
   );
 }
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,17 +2,20 @@ import React from "react";
 
 export default function About() {
   return (
-    <div>
-      <div>
-        <div>
-          <h2>MIND-BLOWING TRAINING THAT CHANGE LIVES</h2>
-          <p>THE HEROICFIT MISSION</p>
-          <p>
-            We help people achieve the body transformation results they
-            want. No matter where you are in the world, our tailored online
-            program gives you the same proven methods, data-driven approach and
-            culture of accounntability that have helped clients achieve
-            remarkable results in and out of the gym globally.
+    <div className="w-full my-32">
+      <div className="max-w-[1000px] mx-auto px-14">
+        <div className="text-center">
+          <p className="text-1xl font-light">THE HEROICFIT MISSION</p>
+          <h2 className="text-3xl font-semibold mt-2">
+            We're On A Mission To Deliver High-Quality Personal Training That's
+            Accessible To Everyone
+          </h2>
+          <p className="text-1xl mt-6">
+            We help people achieve the body transformation results they want. No
+            matter where you are in the world, our tailored online program gives
+            you the same proven methods, data-driven approach and culture of
+            accounntability that have helped clients achieve remarkable results
+            in and out of the gym globally.
           </p>
         </div>
       </div>
