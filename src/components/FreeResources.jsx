1cc6b0c7b7b6bf4f0fdda20f33c4816ccf5b579f
thuannguyen88 <getaholdofthuan@gmail.com>
2022-04-16 17:41:43 +0100

    added tailwind styling for resource cards

--- src/components/FreeResources.jsx
+++ src/components/FreeResources.jsx
@@ -1,30 +1,41 @@
 import React from "react";
+import freeResourceRecipeImage from "../assets/recipe-pack-1.png";
+import freeResourceRecipeImageTwo from "../assets/recipe-pack-2.png";
 
 export default function FreeResources() {
   return (
-    <div className="w-full h-screen bg-zinc-200 flex flex-col justify-between">
-      <div className="flex flex-col justify-center w-full m-auto px-14">
+    <div className="w-full  bg-zinc-200 flex flex-col justify-between">
+      <div className="flex flex-col justify-center my-16 mx-auto px-14 max-w-[1000px]">
         <div className="text-center">
-          <p className="my-2 text-1xl font-light">
+          <p className="text-1xl font-light">
             WANT TO START TRANSFORMING YOUR PHYSIQUE?
           </p>
-          <h2 className="text-3xl font-semibold">
+          <h2 className="text-3xl font-semibold mt-2">
             Get Started With These{" "}
-            <span className=" underline underline-offset-2">Free</span> Resources
+            <span className=" underline underline-offset-2">Free</span>{" "}
+            Resources
           </h2>
         </div>
-        <div>
-          <div>
-            <p>GET STARTED WITH MEAL PLANNING</p>
-            <img alt="/"></img>
-            <h2>Recipe Plan</h2>
-            <button>GIVE IT TO ME</button>
+        <div className="flex flex-col md:flex-row justify-between">
+          <div className="flex flex-col items-left  mt-10 mb-4 mx-auto max-w-[400px]">
+            <img src={freeResourceRecipeImage} alt="/" className="mt-4"></img>
+            <p className="text-1xl font-light mt-6">
+              GET STARTED WITH MEAL PLANNING
+            </p>
+            <h2 className="text-3xl font-bold mt-1 mb-4">
+              A Week Worth Of Quick Nutritious Recipes
+            </h2>
+            <button className="py-4 px-7">GIVE IT TO ME !</button>
           </div>
-          <div>
-            <p>GET STARTED WITH TRAINING PLAN!</p>
-            <img alt="/"></img>
-            <h2>Workout Plan</h2>
-            <button>YES, I WANT THAT!</button>
+          <div className="flex flex-col items-left mt-10 mb-4 mx-auto max-w-[400px] md:ml-16">
+            <img src={freeResourceRecipeImage} alt="/" className="mt-4"></img>
+            <p className="text-1xl font-light mt-6">
+              GET STARTED WITH TRAINING
+            </p>
+            <h2 className="text-3xl font-bold mt-1 mb-4">
+              One Week Workout Programme
+            </h2>
+            <button className="py-4 px-7">YES, I'D LIKE THAT !</button>
           </div>
         </div>
       </div>
