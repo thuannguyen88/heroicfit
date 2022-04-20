36efefa2ab0294945647a2bed2d1401d6b708044
thuannguyen88 <getaholdofthuan@gmail.com>
2022-04-20 18:10:03 +0100

    positioning elements within products component

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function Products() {
   return (
     <div className="bg-slate-200 w-full">
-      <div className="flex flex-col w-max-[1000px] mx-auto">
-        <div className="flex flex-col mt-14">
+      <div className="flex flex-col max-w-[1000px] mx-auto md:flex-row md:justify-around md:py-14">
+        <div className="flex flex-col md:pt-14 mt-14 md:max-w-[40%] p-4 md:max-h-[400px] ">
           <h1 className="text-sm font-light">RESOURCES</h1>
           <h3 className="text-3xl font-bold mt-3">
             Done-For-You Templates & Courses To Help You Grow
@@ -16,8 +16,8 @@ export default function Products() {
             and skyrocket your business growth.
           </p>
         </div>
-        <div className="my-5">
-          <div className="flex flex-col bg-gradient-to-br from-cyan-600 to-sky-900  max-w-[370px] h-[500px] mx-auto">
+        <div className="my-5 md:mr-10 flex flex-col justify-around">
+          <div className="flex flex-col bg-gradient-to-br from-cyan-600 via-sky-900 to-indigo-900 max-w-[370px] h-[500px] mx-auto rounded-md md:h-[400px] ">
             <div className=" p-12 flex h-[100%] justify-between flex-col text-center ">
               <div>
                 <h3 className="">HFK</h3>
@@ -30,12 +30,12 @@ export default function Products() {
                 Includes monthly new recipes, so you'll always have new recipes
                 that keep you on track and tickle your tastebuds
               </p>
-              <button className="bg-slate-200 text-sky-900 p-3 font-medium">
+              <button className="bg-slate-200 text-sky-900 p-3 font-medium border-0">
                 LEARN MORE
               </button>
             </div>
           </div>
-          <div className="flex flex-col bg-gradient-to-tr from-yellow-500 via-orange-500 to-pink-500  max-w-[370px] h-[500px] mx-auto mt-4">
+          <div className="flex flex-col bg-gradient-to-tr from-yellow-500 via-orange-500 to-pink-500  max-w-[370px] h-[500px] mx-auto mt-8 rounded-md md:h-[400px] ">
             <div className=" p-12 flex h-[100%] justify-between flex-col text-center ">
               <div>
                 <h3 className="">HFL</h3>
@@ -48,7 +48,7 @@ export default function Products() {
                 challenging and those gains coming. The most affordable way to
                 get expert personal training.
               </p>
-              <button className="bg-slate-200 text-sky-900 p-3 font-medium">
+              <button className="bg-slate-200 text-orange-500 p-3 font-medium border-0">
                 LEARN MORE
               </button>
             </div>
