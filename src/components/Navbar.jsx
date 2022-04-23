bffb8e9d989cbeecfedb8258b7d193f42191d04b
thuannguyen88 <getaholdofthuan@gmail.com>
2022-04-23 22:46:17 +0100

    re-factor navbar styling to sky blue theme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,14 +8,16 @@ export default function Navbar() {
   };
 
   return (
-    <div className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
+    <div className="w-screen h-[80px] z-10 bg-sky-100 fixed drop-shadow-lg">
       <div className="px-2 flex justify-between items-center w-full h-full">
         <div className="flex items-center">
-          <h1 className="text-3xl font-bold mr-4 sm:text-4xl">HEROICFIT.</h1>
-          <ul className="hidden md:flex ">
-            <li>Free Resources</li>
-            <li>Meal Plans</li>
-            <li>Online Training</li>
+          <h1 className="text-3xl font-bold mr-4 sm:text-3xl">
+            HEROIC<span className="text-sky-500">FIT</span>
+          </h1>
+          <ul className="hidden md:flex font-semibold text-slate-600">
+            <li>Home</li>
+            <li>About</li>
+            <li>Free Stuff</li>
           </ul>
         </div>
         <div className="hidden md:flex pr-4">
@@ -25,19 +27,22 @@ export default function Navbar() {
           <button className="px-4 py-2">Join Now</button>
         </div>
         <div className="md:hidden" onClick={handleClick}>
-          {menuClose ? <MenuIcon className="w-12" /> : <XIcon className="w-6" />}
+          {menuClose ? (
+            <MenuIcon className="w-12" />
+          ) : (
+            <XIcon className="w-6" />
+          )}
           {/* // <MenuIcon className="w-12" /> */}
         </div>
       </div>
 
       <ul className={menuClose ? "hidden" : "absolute w-full px-8 bg-zinc-200"}>
-        <li className="border-b-2 border-zinc-300 w-full">home</li>
-        <li className="border-b-2 border-zinc-300 w-full">about</li>
-        <li className="border-b-2 border-zinc-300 w-full">support</li>
-        <li className="border-b-2 border-zinc-300 w-full">platforms</li>
-        <li className="border-b-2 border-zinc-300 w-full">pricing</li>
+        <li className="border-b-2 border-zinc-300 w-full">Home</li>
+        <li className="border-b-2 border-zinc-300 w-full">About</li>
+        <li className="border-b-2 border-zinc-300 w-full">Free Stuff</li>
+
         <div className="flex flex-col my-2">
-          <button className="bg-transparent text-indigo-600 px-4 py-2 my-5">
+          <button className="bg-transparent text-sky-500 px-4 py-2 my-5">
             Sign In
           </button>
           <button className="px-4 py-2">Sign Up</button>
