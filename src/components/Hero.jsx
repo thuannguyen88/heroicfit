02044b8866d9b159f1eea7e659597f7a7343f043
thuannguyen88 <getaholdofthuan@gmail.com>
2022-05-06 14:12:06 +0100

    adeed all the svg assets for hero section and positioned according to figma handoff

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 import bgImg from "../assets/guy-girl-training.jpg";
+import bgImgOrder from "../assets/hero-image-exercise-order.svg";
+import bgImgChat from "../assets/hero-image-chat.svg";
+import bgImgDots from "../assets/hero-image-dots.svg";
 
 export default function Hero() {
   return (
     <div className="w-full h-screen  flex flex-col justify-between px-8">
-      <div className="flex flex-row max-w-[1240px] m-auto ">
+      <div className="flex flex-row max-w-[1240px] m-auto h-[600px]">
         <div className="flex flex-col justify-center py-8 pr-16 max-w-[700px]">
           <p className="text-2xl font-light">online fitness training</p>
           <h1 className="py-3 text-5xl  font-black tracking-wider md:leading-[1.05]">
@@ -21,8 +24,27 @@ export default function Hero() {
             Get Started
           </button>
         </div>
-        <div className=" flex flex-col max-w-[50%] justify-center">
-          <img src={bgImg} alt="hero_image" className="hidden md:flex " />
+        <div className=" flex flex-col max-w-[50%] justify-center items-end relative">
+          <img
+            src={bgImg}
+            alt="hero_image"
+            className="hidden md:flex max-w-[95%]"
+          />
+          <img
+            src={bgImgOrder}
+            alt="hero_image"
+            className="hidden md:flex absolute top-14 left-16 "
+          />
+          <img
+            src={bgImgChat}
+            alt="hero_image"
+            className="hidden md:flex absolute bottom-0 right-10 "
+          />
+          <img
+            src={bgImgDots}
+            alt="hero_image"
+            className="hidden md:flex absolute bottom-[4.6rem] left-0"
+          />
         </div>
       </div>
     </div>
