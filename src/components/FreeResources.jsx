d195975bfee8809896ab65cd4f5929ca9a43f555
thuannguyen88 <getaholdofthuan@gmail.com>
2022-06-06 21:10:03 +0100

    resizing buttons and font weight in smaller media queries

--- src/components/FreeResources.jsx
+++ src/components/FreeResources.jsx
@@ -26,7 +26,7 @@ export default function FreeResources() {
         </div>
       </div>
       <div className="w-full flex flex-row">
-        <div className="bg-cyan-100 py-10 w-[100%] sm:w-[60%] px-10 rounded-lg">
+        <div className="bg-cyan-100 py-10 w-[100%] lg:w-[60%] px-10 rounded-lg">
           <div className="flex flex-col justify-center my-16  mx-auto max-w-[567px] ">
             <h3 className="text-3xl  text-slate-800 font-bold">
               Snag Your Free Client Meal Plan
@@ -51,20 +51,20 @@ export default function FreeResources() {
               </li>
             </ul>
             <div className="flex justify-center my-5">
-              <button className="mr-10 py-4 px-7">I'M HIGH PROTEIN</button>
-              <button className="py-4 px-7">I'M PLANT BASED</button>
+              <button className="mr-10 sm:py-4 sm:px-7 py-2 px-3 font-semibold">
+                I'M HIGH PROTEIN
+              </button>
+              <button className="sm:py-4 sm:px-7 py-2 px-3 bg-transparent text-sky-500 font-semibold">
+                I'M PLANT BASED
+              </button>
             </div>
           </div>
         </div>
-        <div className="w-[0] sm:w-[48%] flex justify-center my-auto lg:my-0 lg:w-[27%] lg:relative">
-          <div className=" hidden sm:inline-block lg:absolute lg:top-4 lg:left-0 ml-[0] lg:ml-[-70px]">
-            <img
-              src={mealPlanImageOne}
-              alt="/"
-              className="sm:h-[30rem] lg:h-[20rem]"
-            ></img>
+        <div className="w-[0]  flex justify-center my-auto lg:my-0 lg:w-[27%] lg:relative">
+          <div className="hidden absolute top-4 left-0 lg:ml-[-70px] lg:inline-block">
+            <img src={mealPlanImageOne} alt="/" className=" lg:h-[20rem]"></img>
           </div>
-          <div className="absolute bottom-0 right-10 mb-[-25px] hidden lg:inline-block">
+          <div className="lg:absolute lg:bottom-0 lg:right-10 lg:mb-[-25px]  sm:hidden lg:inline-block">
             <img src={mealPlanImageTwo} alt="/" className="h-[23rem] "></img>
           </div>
           <div className="absolute top-0 right-[8rem] hidden lg:inline-block">
