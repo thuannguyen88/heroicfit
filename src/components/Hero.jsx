9aa6166c1900a9ddbaead4afca495439be623463
thuannguyen88 <getaholdofthuan@gmail.com>
2022-06-06 19:12:24 +0100

    hero section added height so it takes up whole page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,8 +7,8 @@ import bgImgDots from "../assets/hero-image-dots.svg";
 
 export default function Hero() {
   return (
-    <div className="w-screen ">
-      <div className="flex md:flex-row max-w-[1240px] m-auto min-h-[600px] px-5 pt-[50px]">
+    <div className="w-screen h-screen flex flex-col ">
+      <div className="flex md:flex-row max-w-[1240px] m-auto min-h-[600px] px-5 ">
         <div className="flex flex-col justify-center py-8 max-w-[710px] text-center md:text-left align-middle">
           <p className="text-2xl font-light">online fitness training</p>
           <h1 className="py-3 text-4xl md:text-5xl  font-black tracking-[0.07em] md:leading-[1.05]">
